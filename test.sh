1045eb78d3c69c1cefbdedaafd69c985e3c91dff
Nicholas Moore <nicholasvmoore@gmail.com>
2015-09-16 21:43:02 +0000

    major breakthrough

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -ti --entrypoint=/bin/bash -v /mnt/lun00/coreos00/confd:/etc/confd:rw -P confd 
+docker run -ti --rm --entrypoint=/bin/bash -v /mnt/lun00/coreos00/confd:/etc/confd:rw -P --name=confd confd
