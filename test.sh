8530f46b81a53d6f5c03193f886c3bc853edb530
Nicholas Moore <nicholasvmoore@gmail.com>
2015-09-17 19:07:08 +0000

    lots of changes

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -ti --rm --entrypoint=/bin/bash -v /mnt/lun00/coreos00/confd:/etc/confd:rw -P --name=confd confd
+docker run -ti --rm --entrypoint=/bin/bash -v /mnt/lun00/coreos00/confd:/etc/confd:rw -v /mnt/lun00/coreos00/wordpress/dev:/php:rw -P --name=confd confd
